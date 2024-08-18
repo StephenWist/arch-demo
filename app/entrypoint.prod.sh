fb0cb14e218117905e61ea2d7f6ec0106656f814
Stephen Wist <stphnwist@gmail.com>
2024-08-18 14:16:25 -0400

    Add comment

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-# verifies Postgres is healthy beofre running migrations and Django
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    # while net cat doesn't have a response from Postgres
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
-
-python manage.py flush --no-input
-python manage.py migrate
 
 exec "$@"
