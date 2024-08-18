10e10056c52a51a5cf622fd40c324995ff345d06
Stephen Wist <stphnwist@gmail.com>
2024-08-18 13:47:29 -0400

    Get entrypoint running I think

--- app/arch_demo/urls.py
+++ app/arch_demo/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    # path("", views.index, name="index"),
     path('admin/', admin.site.urls),
+    # path('/', views.landing, name='landing')
 ]
